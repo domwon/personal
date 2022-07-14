30593b31378c377f73c833dbcb958543fd5f47a6
Dom <domnguyen531@gmail.com>
2022-07-14 13:53:08 -0500

    Add Tailwind & DaisyUI - Deploy to new Firebase project dominic-nguyen instead of domkhac-personal

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,49 @@
-import { useState } from 'react'
-// import logo from './logo.svg'
-import logo from './images/dom-profile.jpg'
+import logo from './images/dom-profile.jpg';
 import './App.css'
-import { BsTwitter, BsLinkedin, BsGithub } from 'react-icons/bs';
-
+import { BsTwitter, BsLinkedin, BsGithub, BsEnvelopeFill } from 'react-icons/bs';
 
 const links = [
   { id: 1, name: 'Twitter', url: 'https://twitter.com/domknguyen' },
-  { id: 2, name: 'LinkedIn', url: 'https://www.linkedin.com/in/dominickhacnguyen/' },
+  { id: 2, name: 'LinkedIn', url: 'https://www.linkedin.com/in/dominickhacnguyen' },
   { id: 3, name: 'GitHub', url: 'https://github.com/domwon' },
+  { id: 3, name: 'Mail', url: 'mailto:domkhac@gmail.com' },
 ];
 
 function App() {
 
   return (
-    <div className="App font-face-rc">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Dominic Nguyen</h1>
-        <p>
-          Hi! I'm learning Web3 and building <a className="link" href="https://xplorepod.com" target="_blank"
-            rel="noopener noreferrer">XplorePod</a> to help people explore and do more of what makes them happy.
-        </p>
-        <p>
-          {links && links.map(({ id, name, url }) => {
-            return <a
-              key={id}
-              className="App-link"
-              href={url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {name === 'Twitter' ? <BsTwitter /> : null}
-              {name === 'LinkedIn' ? <BsLinkedin /> : null}
-              {name === 'GitHub' ? <BsGithub /> : null}
-            </a>
-          })}
-        </p>
-      </header>
-    </div>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={logo} className="rounded-xl shadow-2xl mask mask-squircle mx-8 pb-5 max-w-1" />
+
+        <div>
+          <h1 className="text-4xl font-bold">Dominic Nguyen</h1>
+
+          <p className="py-6 max-w-sm">Hi there! I am a full-stack developer and data scientist currently building <a className="link link-accent link-hover font-bold" href="https://xplorepod.com" target="_blank"
+            rel="noopener noreferrer">XplorePod</a> to help people do more of what makes them happy.
+          </p>
+
+          <ul className="menu menu-horizontal bg-base-100 p-2 rounded-box">
+            {links && links.map(({ id, name, url }) => {
+              return <li>
+                <a
+                  key={id}
+                  className="link link-accent"
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {name === 'Twitter' ? <BsTwitter /> : null}
+                  {name === 'LinkedIn' ? <BsLinkedin /> : null}
+                  {name === 'GitHub' ? <BsGithub /> : null}
+                  {name === 'Mail' ? <BsEnvelopeFill /> : null}
+                </a>
+              </li>
+            })}
+          </ul>
+        </div>
+      </div>
+    </div >
   )
 }
 
